95cd63df407808402d453b6d19aaaa1371d4b9ff
mostafa <mostafameerzad@gmail.com>
2024-10-26 20:18:43 +0430

    Show Issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingIssueDetailPage = () => {
+  return (
+    <div>Loading issue detail...</div>
+  )
+}
+
+export default LoadingIssueDetailPage
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingNewIssuePage = () => {
+  return (
+    <div>Loading new issue...</div>
+  )
+}
+
+export default LoadingNewIssuePage
