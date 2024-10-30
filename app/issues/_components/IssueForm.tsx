9230d6b54460780004cb6b7643e24399340bd38c
mostafa <mostafameerzad@gmail.com>
2024-10-30 20:15:25 +0430

    Improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -7,17 +7,13 @@ import { Issue } from "@prisma/client";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { BiInfoCircle } from "react-icons/bi";
+import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
-
 type IssueFormType = z.infer<typeof issueSchema>;
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 
 interface Props {
   issue?: Issue;
@@ -44,7 +40,7 @@ const IssueForm = ({ issue }: Props) => {
       else await axios.post("/api/issues", data);
 
       router.push("/issues");
-      router.refresh()
+      router.refresh();
     } catch (error) {
       setIsSubmitting(false);
       setError("An unexpected error occurred!");
