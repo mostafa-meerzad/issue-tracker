962faee533761d4b0d1f5aade0547c281d1a1870
mostafa <mostafameerzad@gmail.com>
2024-11-05 17:23:10 +0430

    Refactor the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -15,14 +15,6 @@ import {
   Text,
 } from "@radix-ui/themes";
 const Navbar = () => {
-  const currentPath = usePathname(); // this hook comes with Next.js that allows us to get the current-path path-name
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
-  ];
-
-  const { status, data: session } = useSession();
-
   return (
     <nav className=" border-b mb-5 px-5 py-5">
       <Container>
@@ -31,55 +23,72 @@ const Navbar = () => {
             <Link href={"/"}>
               <AiFillBug />
             </Link>
-
-            <ul className="flex gap-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    href={link.href}
-                    className={classnames({
-                      "text-zinc-500": currentPath !== link.href,
-                      "text-zinc-900": currentPath === link.href,
-                      " hover:text-zinc-900 transition-colors": true,
-                    })}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
           <Flex>
-            <Box>
-              {status === "authenticated" && (
-                <DropdownMenu.Root>
-                  <DropdownMenu.Trigger>
-                    <Avatar
-                      src={session.user?.image!}
-                      fallback="?"
-                      radius="full"
-                      className="cursor-pointer"
-                    />
-                  </DropdownMenu.Trigger>
-
-                  <DropdownMenu.Content >
-                    <DropdownMenu.Label>
-                      <Text size={"2"}>{session.user?.email}</Text>
-                    </DropdownMenu.Label>
-                    <DropdownMenu.Item>
-                      <Link href={"/api/auth/signout"}>Log out</Link>
-                    </DropdownMenu.Item>
-                  </DropdownMenu.Content>
-                </DropdownMenu.Root>
-              )}
-              {status === "unauthenticated" && (
-                <Link href={"/api/auth/signin"}>Log in</Link>
-              )}
-            </Box>
+            <AuthStatus />
           </Flex>
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname(); // this hook comes with Next.js that allows us to get the current-path path-name
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
+  return (
+    <ul className="flex gap-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={classnames({
+              "nav-link":true,
+              "!text-zinc-900": currentPath === link.href,
+            })}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated")
+    return <Link href={"/api/auth/signin"} className="nav-link">Log in</Link>;
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            radius="full"
+            className="cursor-pointer"
+          />
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size={"2"}>{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href={"/api/auth/signout"}>Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
