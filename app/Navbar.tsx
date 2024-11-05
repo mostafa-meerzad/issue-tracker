88f674ba5817d938a72e21ba08384cd559b1c90e
mostafa <mostafameerzad@gmail.com>
2024-11-05 14:12:47 +0430

    Change the layout of the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 const Navbar = () => {
   const currentPath = usePathname(); // this hook comes with Next.js that allows us to get the current-path path-name
   const links = [
@@ -15,36 +15,45 @@ const Navbar = () => {
   ];
 
   const { status, data: session } = useSession();
-  
+
   return (
-    <nav className="flex items-center gap-x-5 p-5 mb-5 h-14 border">
-      <Link href={"/"}>
-        <AiFillBug />
-      </Link>
-      <ul className="flex gap-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={classnames({
-                "text-zinc-500": currentPath !== link.href,
-                "text-zinc-900": currentPath === link.href,
-                " hover:text-zinc-900 transition-colors": true,
-              })}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href={"/api/auth/signout"}>Log out</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href={"/api/auth/signin"}>Log in</Link>
-        )}
-      </Box>
+    <nav className=" border-b mb-5 px-5 py-5">
+      <Container>
+      <Flex justify={"between"}>
+        <Flex gap={"3"} align={"center"}>
+          <Link href={"/"}>
+            <AiFillBug />
+          </Link>
+
+          <ul className="flex gap-x-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  href={link.href}
+                  className={classnames({
+                    "text-zinc-500": currentPath !== link.href,
+                    "text-zinc-900": currentPath === link.href,
+                    " hover:text-zinc-900 transition-colors": true,
+                  })}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Flex>
+        <Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Link href={"/api/auth/signout"}>Log out</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href={"/api/auth/signin"}>Log in</Link>
+            )}
+          </Box>
+        </Flex>
+      </Flex>
+      </Container>
     </nav>
   );
 };
